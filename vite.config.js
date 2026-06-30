55352179b12f73b8193492bd14a7b1d9206116a3
mecom8489 <mecom8489@gmail.com>
2026-06-30 11:42:42 +0530

    fix: update base path to root in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Capitalgrow-EduLoan/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
